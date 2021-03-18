d8421dc1f77db3632c22b144ad5ce6e1d3f24a6d
Daphne Link <daphnelink@gmail.com>
2021-03-18 15:27:59 -0700

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 //var Port = ":4000" //local test
@@ -16,7 +17,20 @@ func main() {
 		log.Println("$PORT must be set, defaulting to 9000")
 		port = "9000"
 	}
-	http.HandleFunc("/", ServePages)
-	fmt.Println("Listening @:", port)
-	log.Println(http.ListenAndServe(":"+port, nil))
+
+	/*
+		http.HandleFunc("/", ServePages)
+		fmt.Println("Listening @:", port)
+		log.Println(http.ListenAndServe(":"+port, nil))
+	*/
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("../assets/pages/*.html")
+	router.Static("../assets/pages", "pages")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.Run(":" + port)
 }
