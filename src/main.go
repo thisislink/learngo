03dc8ba75331a2c63e41c5d6ee56614bad88c819
Daphne Link <daphnelink@gmail.com>
2021-03-18 11:30:48 -0700

    updates

--- src/main.go
+++ src/main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-var Port = ":4000"
+//var Port = ":4000" //local test
 
 func main() {
-	http.HandleFunc("/", ServePages)
-	fmt.Println("Listening @:", Port)
-	log.Fatal(http.ListenAndServe(Port, nil))
+	port := os.Getenv("PORT")
 
+	if port == "" {
+		log.Println("$PORT must be set, defaulting to 4000")
+		port = ":4000"
+	}
+	http.HandleFunc("/", ServePages)
+	fmt.Println("Listening @:", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
